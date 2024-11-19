62c3aa8f40cab6111d3fc85411a418a409b702d9
Pedro Guerrero <pguerrero86@gmail.com>
2024-11-18 22:01:21 -0300

    add queue data structure

---
+++ src/js/Queue.js
@@ -0,0 +1,17 @@
+export class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(item) {
+    this.queue.push(item);
+  }
+
+  dequeue() {
+    return this.queue.shift();
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+}
